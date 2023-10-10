a331e90eafe36d6ab03a4e347f554ecaca22e11c
han.wei <han.wei.work2023@gmail.com>
2023-10-10 15:34:09 +0800

    project init

---
+++ src/main/java/com/weiyiyi/ditar/DitarApplication.java
@@ -0,0 +1,13 @@
+package com.weiyiyi.ditar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DitarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DitarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/weiyiyi/ditar/DitarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weiyiyi.ditar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DitarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
